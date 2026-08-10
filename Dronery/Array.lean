c7cc35709330ed8a6da64ea5d51983ad8e5c9fe1
jintsi <43089859+jintsi@users.noreply.github.com>
2026-08-10 14:28:28 +0200

    some more list/array methods

--- Dronery/Array.lean
+++ Dronery/Array.lean
@@ -1,4 +1,4 @@
-import Mathlib.Algebra.Group.Basic
+import Dronery.List
 
 namespace Array
 
@@ -8,11 +8,19 @@ def revRange (n : Nat) : Array Nat := ofFn (n := n) fun i => i.rev
 theorem revRange_def : revRange n = (range n).reverse := by
   ext <;> simp [revRange]; omega
 
+@[simp]
+theorem toList_revRange : (revRange n).toList = List.revRange n := by
+  simp [revRange_def, List.revRange_def]
+
 /-- Construct an array of all elements of `Fin n` in decreasing order. -/
 def revFinRange (n : Nat) : Array (Fin n) := ofFn Fin.rev
 
 theorem revFinRange_def : revFinRange n = (Array.finRange n).reverse := by
   ext <;> simp [revFinRange]; omega
+
+@[simp]
+theorem toList_revFinRange : (revFinRange n).toList = List.revFinRange n := by
+  simp [revFinRange]; rfl
 
 /-- Construct an array of numbers of size `len`, decreasing by `step` at each element and ending
 at `stop`, that is, `#[stop+(len-1)*step, ..., stop+step, stop]`. -/
@@ -23,6 +31,95 @@ theorem revRange'_def {stop len step} :
     revRange' stop len step = (range' stop len step).reverse := by
   ext i h <;> simp [revRange']; simp [revRange'] at h
   rw [Nat.add_sub_assoc, ← Nat.mul_sub]; apply Nat.mul_le_mul_left; omega
+
+@[simp]
+theorem toList_revRange' {stop len step} :
+    (revRange' stop len step).toList = List.revRange' stop len step := by
+  simp [revRange'_def, List.revRange'_def]
+
+/-- Fold an array from left to right as with `foldlM`, but the combining function also receives
+each element's index as a parameter. Monadic variant of `foldlIdx`. -/
+@[inline]
+def foldlIdxM [Monad m] (f : ℕ → β → α → m β) (init : β) (as : Array α) : m β :=
+  Prod.snd <$> as.foldlM (fun (n, acc) a => Prod.mk n.succ <$> f n acc a) (0, init)
+
+@[simp]
+theorem _root_.List.foldlIdxM_toArray [Monad m] (f : ℕ → β → α → m β) (init : β) (l : List α) :
+    l.toArray.foldlIdxM f init = l.foldlIdxM' f init := by unfold foldlIdxM List.foldlIdxM'; simp
+
+theorem foldlIdxM_eq_foldlM_zipIdx [Monad m] [LawfulMonad m] {f : ℕ → β → α → m β} {as : Array α} :
+    as.foldlIdxM f i = as.zipIdx.foldlM (fun b ai => f ai.snd b ai.fst) i := by
+  cases as; simp; apply List.foldlIdxM'_eq_foldlM_zipIdx
+
+/-- Fold an array from right to left as with `foldrM`, but the combining function also receives
+each element's index as a parameter. Monadic variant of `foldrIdx`. -/
+@[inline]
+def foldrIdxM [Monad m] (f : ℕ → α → β → m β) (init : β) (as : Array α) : m β :=
+  Prod.snd <$> as.foldrM (fun a (n, acc) => Prod.mk n.pred <$> f n.pred a acc) (as.size, init)
+
+@[simp]
+theorem _root_.List.foldrIdxM_toArray [Monad m] (f : ℕ → α → β → m β) (init : β) (l : List α) :
+    l.toArray.foldrIdxM f init = l.foldrIdxM' f init := by unfold foldrIdxM List.foldrIdxM'; simp
+
+theorem foldrIdxM_eq_foldrM_zipIdx [Monad m] [LawfulMonad m] {f : ℕ → α → β → m β} {as : Array α} :
+    as.foldrIdxM f i = as.zipIdx.foldrM (fun ai b => f ai.snd ai.fst b) i := by
+  cases as; simp; apply List.foldrIdxM'_eq_foldrM_zipIdx
+
+/-- Fold an array from left to right as with `foldl`, but the combining function also receives
+each element's index as a parameter. -/
+@[inline]
+def foldlIdx (f : ℕ → β → α → β) (init : β) (as : Array α) : β :=
+  Id.run <| foldlIdxM (fun n b a => pure (f n b a)) init as
+
+@[simp]
+theorem _root_.List.foldlIdx_toArray {l : List α} : l.toArray.foldlIdx f i = l.foldlIdx f i := by
+  unfold foldlIdx; simp; generalize 0 = s; induction l generalizing i s with simp_all
+
+theorem foldlIdx_eq_foldl_zipIdx {as : Array α} :
+    as.foldlIdx f i = as.zipIdx.foldl (fun b ai => f ai.snd b ai.fst) i := by
+  cases as; simp; apply List.foldlIdx_eq_foldl_zipIdx
+
+/-- Fold an array from right to left as with `foldr`, but the combining function also receives
+each element's index as a parameter. -/
+@[inline]
+def foldrIdx (f : ℕ → α → β → β) (init : β) (as : Array α) : β :=
+  Id.run <| foldrIdxM (fun n a b => pure (f n a b)) init as
+
+@[simp]
+theorem _root_.List.foldrIdx_toArray {l : List α} : l.toArray.foldrIdx f i = l.foldrIdx f i := by
+  unfold foldrIdx; simp; generalize 0 = s; induction l generalizing i s with simp_all
+
+theorem foldrIdx_eq_foldr_zipIdx {as : Array α} :
+    as.foldrIdx f i = as.zipIdx.foldr (fun ai b => f ai.snd ai.fst b) i := by
+  cases as; simp; apply List.foldrIdx_eq_foldr_zipIdx
+
+/-- Applies a monadic function that returns an `Option` to each element of an array along with the
+index at which that element is found, collecting the non-`none` values. -/
+@[inline]
+def filterMapIdxM [Monad m] (f : ℕ → α → m (Option β)) (as : Array α) : m (Array β) :=
+  as.foldlIdxM (init := #[]) fun i bs a => (fun
+    | some b => bs.push b
+    | none   => bs) <$> f i a
+
+@[simp]
+theorem filterMapIdxM_eq_filterMapM_zipIdx [Monad m] [LawfulMonad m] {f : ℕ → α → m (Option β)}
+    {as : Array α} : as.filterMapIdxM f = as.zipIdx.filterMapM fun ai => f ai.snd ai.fst := by
+  rw [filterMapIdxM, foldlIdxM_eq_foldlM_zipIdx, filterMapM]; simp [← bind_pure_comp]
+  congr!; split <;> rfl
+
+/-- Applies a function that returns an `Option` to each element of an array along with the index at
+which that element is found, collecting the non-`none` values. -/
+@[inline]
+def filterMapIdx (f : ℕ → α → Option β) (as : Array α) : Array β :=
+  as.foldlIdx (init := #[]) fun i bs a => match f i a with
+    | some b => bs.push b
+    | none   => bs
+
+@[simp]
+theorem filterMapIdx_eq_filterMap_zipIdx {f : ℕ → α → Option β} {as : Array α} :
+    as.filterMapIdx f = as.zipIdx.filterMap fun ai => f ai.snd ai.fst := by
+  rw [filterMap, ← filterMapIdxM_eq_filterMapM_zipIdx (f := fun i a => pure (f i a)),
+    filterMapIdxM, filterMapIdx, foldlIdx]; simp_rw [map_pure]
 
 /-- Computes the sum of `f` applied to elements of the array. Note that it does a left fold,
 as opposed to `Array.sum` which does a right fold. -/
--- Dronery/List.lean
+++ Dronery/List.lean
@@ -54,17 +54,97 @@ theorem revRange'_def {stop len step : Nat} :
   | succ l ih => rw [revRange'.go, l.add_comm, ← i.add_assoc, ← ih, Nat.mul_add_one, Nat.add_assoc,
     range'_concat]; simp
 
+/-- An better version of Mathlib's `foldlIdxM` (really, use this one). The indexes can optionally
+begin at some `start` (by default 0). -/
+@[inline]
+def foldlIdxM' [Monad m] (f : ℕ → β → α → m β) (init : β) (l : List α) (start := 0) : m β :=
+  Prod.snd <$> l.foldlM (fun (n, acc) a => Prod.mk n.succ <$> f n acc a) (start, init)
+
+@[simp]
+theorem foldlIdxM'_nil [Monad m] [LawfulApplicative m] {f : ℕ → β → α → m β} :
+    [].foldlIdxM' f i s = pure i := by unfold foldlIdxM'; simp
+
+@[simp]
+theorem foldlIdxM'_cons [Monad m] [LawfulMonad m] {f : ℕ → β → α → m β} :
+    (a :: l).foldlIdxM' f i s = f s i a >>= (l.foldlIdxM' f · (s + 1)) := by
+  unfold foldlIdxM'; simp
+
+/-- Can't make it `@[csimp]` because it requires `LawfulMonad`. -/
+theorem foldlIdxM_eq_foldlIdxM' [Monad m] [LawfulMonad m] {f : ℕ → β → α → m β} {init : β}
+    {l : List α} : l.foldlIdxM f init = l.foldlIdxM' f init := by
+  unfold foldlIdxM; generalize 0 = start
+  induction l generalizing start init with
+  | nil => simp
+  | cons a l ih =>
+    simp [← ih]; clear ih; generalize f start init a = x
+    induction l generalizing start x with
+    | nil => simp
+    | cons a' l ih => simp; rw [ih, bind_assoc]; simp [← ih]
+
+theorem foldlIdxM'_eq_foldlM_zipIdx [Monad m] [LawfulMonad m] {f : ℕ → β → α → m β} {l : List α} :
+    l.foldlIdxM' f i s = (l.zipIdx s).foldlM (fun b ai => f ai.snd b ai.fst) i := by
+  induction l generalizing s i with simp_all
+
+/-- An better version of Mathlib's `foldrIdxM` (really, use this one). The indexes can optionally
+begin at some `start` (by default 0). -/
+@[inline]
+def foldrIdxM' [Monad m] (f : ℕ → α → β → m β) (init : β) (l : List α) (start := 0) : m β :=
+  Prod.snd <$> l.foldrM (fun a (n, acc) => Prod.mk n.pred <$> f n.pred a acc) (start + l.length, init)
+
+@[simp]
+theorem foldrIdxM'_nil [Monad m] [LawfulApplicative m] {f : ℕ → α → β → m β} :
+    [].foldrIdxM' f i s = pure i := by unfold foldrIdxM'; simp
+
+@[simp]
+theorem foldrIdxM'_cons [Monad m] [LawfulMonad m] {f : ℕ → α → β → m β} :
+    (a :: l).foldrIdxM' f i s = l.foldrIdxM' f i (s + 1) >>= f s a := by
+  unfold foldrIdxM'
+  suffices (a :: l).foldrM _ _ = l.foldrM _ _ >>= fun x : ℕ × β => Prod.mk s <$> f s a x.snd by
+    convert congrArg (Prod.snd <$> ·) this; simp; rfl
+  simp; induction l generalizing s a with
+  | nil => simp
+  | cons a' l ih =>
+    rw [foldrM_cons, length_cons, ← s.add_assoc, s.add_right_comm, ih]; symm
+    rw [foldrM_cons, ih]; simp
+
+/-- Can't make it `@[csimp]` because it requires `LawfulMonad`. -/
+theorem foldrIdxM_eq_foldrIdxM' [Monad m] [LawfulMonad m] {f : ℕ → α → β → m β} {init : β}
+    {l : List α} : foldrIdxM f init l = foldrIdxM' f init l := by
+  unfold foldrIdxM; generalize 0 = start; induction l generalizing start with simp_all
+
+theorem foldrIdxM'_eq_foldrM_zipIdx [Monad m] [LawfulMonad m] {f : ℕ → α → β → m β} {l : List α} :
+    l.foldrIdxM' f i s = (l.zipIdx s).foldrM (fun ai b => f ai.snd ai.fst b) i := by
+  induction l generalizing s with simp_all
+
+/-- Applies a monadic function that returns an `Option` to each element of a list along with the
+index at which that element is found, collecting the non-`none` values. -/
+@[inline]
+def filterMapIdxM [Monad m] (f : ℕ → α → m (Option β)) (l : List α) : m (List β) :=
+  reverse <$> l.foldlIdxM' (fun i bs a => (fun o => o.toList ++ bs) <$> f i a) []
+
+@[simp]
+theorem filterMapIdxM_eq_filterMapM_zipIdx [Monad m] [LawfulMonad m] {f : ℕ → α → m (Option β)}
+    {l : List α} : l.filterMapIdxM f = l.zipIdx.filterMapM fun ai => f ai.snd ai.fst := by
+  simp [filterMapIdxM, filterMapM]; generalize [] = acc, 0 = s
+  induction l generalizing acc s with
+  | nil => simp [filterMapM.loop]
+  | cons a l ih => simp [ih, filterMapM.loop]; apply bind_congr; rintro (_ | _) <;> simp
+
+@[simp]
+theorem _root_.Option.reverse_toList (o : Option α) : o.toList.reverse = o.toList := by
+  cases o <;> simp
+
 /-- Applies a function that returns an `Option` to each element of a list along with the index at
 which that element is found, collecting the non-`none` values. -/
 @[inline]
-def filterMapIdx (f : ℕ → α → Option β) (as : List α) : List β :=
-  as.foldrIdx (fun i a bs => (f i a).toList ++ bs) []
+def filterMapIdx (f : ℕ → α → Option β) (l : List α) : List β :=
+  reverse <| l.foldlIdx (fun i bs a => (f i a).toList ++ bs) []
 
 @[simp]
-theorem filterMapIdx_eq_filterMap_zipIdx {f : ℕ → α → Option β} {as : List α} :
-    as.filterMapIdx f = as.zipIdx.filterMap fun (a, i) => f i a := by
-  simp [filterMapIdx, foldrIdx_eq_foldr_zipIdx, filterMap_eq_flatMap_toList, ← flatMap_id,
-    flatMap_map]
+theorem filterMapIdx_eq_filterMap_zipIdx {f : ℕ → α → Option β} {l : List α} :
+    l.filterMapIdx f = l.zipIdx.filterMap fun ai => f ai.snd ai.fst := by
+  simp [filterMapIdx, foldlIdx_eq_foldl_zipIdx, filterMap_eq_flatMap_toList, ← flatMap_id,
+    reverse_flatMap, flatMap_map]
 
 /-- Computes the sum of `f` applied to elements of the list. Note that it does a left fold,
 as opposed to regular `List.sum` which does a right fold. -/
